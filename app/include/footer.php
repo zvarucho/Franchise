4eb6fbd6ec74c471701f56cf464424db49e11413
Alex Zvaruch <number3look@gmail.com>
2023-03-16 21:52:16 +0200

    The include files have been created

---
+++ app/include/footer.php
@@ -0,0 +1,87 @@
+<footer id="footer" class="footer">
+    <div class="footer__main main-footer">
+        <div class="main-footer__container">
+            <div class="main-footer__column">
+                <div class="main-footer__logo">
+                    <img src="" alt="logo">
+                </div>
+                <div class="main-footer__text text">
+                    <p>
+                        Продукт Franchise надає якісні послуги своїм покупцям. Який має досвід у цій сфері понад 6 місяців та допоміг заробити понад 30-ьом учням. Чекаємо тебе в нашій команді!
+                    </p>
+                </div>
+                <div class="main-footer__social social">
+                    <a href="" class="social__link">
+                        <img src="assets/img/social/linkedin.svg" alt="social">
+                    </a>
+                    <a href="" class="social__link">
+                        <img src="assets/img/social/twitter.svg" alt="social">
+                    </a>
+                    <a href="" class="social__link">
+                        <img src="assets/img/social/facebook.svg" alt="social">
+                    </a>
+                    <a href="" class="social__link">
+                        <img src="assets/img/social/YouTube.svg" alt="social">
+                    </a>
+                    <a href="" class="social__link">
+                        <img src="assets/img/social/instagram.svg" alt="social">
+                    </a>
+                </div>
+            </div>
+            <div class="main-footer__column">
+                <div class="main-footer__label">Навігація</div>
+                <nav class="main-footer__menu menu-footer">
+                    <ul class="menu-footer__list">
+                        <li class="menu-footer__item">
+                            <a href="" class="menu-footer__link">Головна</a>
+                        </li>
+                        <li class="menu-footer__item">
+                            <a href="" class="menu-footer__link">Про автора</a>
+                        </li>
+                        <li class="menu-footer__item">
+                            <a href="" class="menu-footer__link">Франшиза</a>
+                        </li>
+                        <li class="menu-footer__item">
+                            <a href="" class="menu-footer__link">Відгуки</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="main-footer__column">
+                <div class="main-footer__label">Untility Pages</div>
+                <nav class="main-footer__menu menu-footer">
+                        <ul class="menu-footer__list">
+                            <li class="menu-footer__item">
+                                <a href="" class="menu-footer__link">Головна</a>
+                            </li>
+                            <li class="menu-footer__item">
+                                <a href="" class="menu-footer__link">Про автора</a>
+                            </li>
+                            <li class="menu-footer__item">
+                                <a href="" class="menu-footer__link">Франшиза</a>
+                            </li>
+                            <li class="menu-footer__item">
+                                <a href="" class="menu-footer__link">Відгуки</a>
+                            </li>
+                        </ul>
+                </nav>
+                </div>
+            <div class="main-footer__column">
+                <div class="main-footer__label">Зв'язатися</div>
+                <div class="main-footer__text text">Залиши свій відгук або пораду щодо продукту.</div>
+                <form action="#" class="login">
+                    <div class="login__div">
+                        <input type="email" placeholder="Введіть свій email" required class="login__input"></input>
+                        <textarea rows="4" name="message" class="login__textarea" placeholder="Твій відгук..." required></textarea>
+                    </div>
+                    <button type="submit" class="login__button button">Надіслати</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="footer__bottom bottom-footer">
+        <div class="bottom-footer__container">
+            <div class="bottom-footer__copy">© Franchise 2023</div>
+        </div>
+    </div>
+</footer>
---
+++ app/include/header.php
@@ -0,0 +1,52 @@
+<header class="header">
+            <div class="header__container">
+                <a href="/" class="header__logo">
+                    <img src="assets/img/logo4.svg" alt="logo">
+                </a>
+                <nav class="header__menu menu">
+                    <div class="menu__body">
+                        <ul class="menu__list">
+                            <li class="menu__item">
+                                <a href="#main" class="menu__link">Головна</a>
+                            </li>
+                            <li class="menu__item">
+                                <a href="#main" class="menu__link">Про автора</a>
+                            </li>
+                            <li class="menu__item">
+                                <a href="#faq" class="menu__link">Франшиза</a>
+                            </li>
+                            <li class="menu__item">
+                                <a href="#testimonials" class="menu__link">Відгуки</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <a href="#" class="header__cart cart-header">
+                    <span class="cart-header__text">Придбано</span>
+                    <span class="cart-header__quantity">3</span>
+                </a>
+                <a href="#pricing" class="header__button button">Придбати</a>
+                <div class="dropdown">
+                    <a href="log.html" class="dropdown__account">
+                        <img class="account-img" src="assets/img/account-white.svg" alt="login">
+                        <button class="dropbtn">Кабінет</button>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="#">Адмін панель</a>
+                        <a href="#">Вихід</a>
+                    </div>
+                </div>
+                <a href="" class="header__language">en</a>  
+                <!-- <a href="" class="account">
+                    <i class="fa fa-user"></i>
+                    Кабинет
+                    <ul class="log-account">
+                        <li><a href="#">Админ панель</a></li>
+                        <li><a href="#">Выход</a></li>
+                    </ul>
+                </a> -->
+                <button class="icon-menu" type="button">
+                    <span></span>
+                </button>
+            </div>
+</header>
